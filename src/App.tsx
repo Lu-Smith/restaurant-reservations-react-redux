b14589f24b8a87bd02693658abc3059af744016e
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-08-13 19:00:15 +0100

    Added basic structure and css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import { GiVikingHelmet } from "react-icons/gi";
+import "./App.css";
+
 
 function App() {
   return (
     <div className="App">
-     Hello World
+      
+      <h1><GiVikingHelmet className="viking-helmet"/>iking Table</h1>
+      <div className="container">
+        <div className="reservation-container">
+          <div>
+            <h5 className="reservation-header">Reservations</h5>
+            <div className="reservation-cards-container">
+              <div className="reservation-card-container">Laith Harb</div>
+            </div>
+          </div>
+          <div className="reservation-input-container">
+            <input />
+            <button>Add</button>
+          </div>
+        </div>
+        <div className="customer-food-container">
+          <div className="customer-food-card-container">
+            <p>Selena Gomez</p>
+            <div className="customer-foods-container">
+              <div className="customer-food"></div>
+              <div className="customer-food-input-container">
+                <input />
+                <button>Add</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
