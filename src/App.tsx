d82fb0c24340925ffe12eb7aeb3ec2673618170d
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-08-14 11:53:59 +0100

    Created CustomerCard.tsx and customerSlice.ts
    Installed "@types/uuid": "^8.3.4", and "uuid": "^8.3.2",

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { GiVikingHelmet } from "react-icons/gi";
 import { useSelector, useDispatch } from "react-redux";
 import "./App.css";
+import CustomerCard from "./app/components/CustomerCard";
 import ReservationCard from "./app/components/ReservationCard";
 import { RootState } from "./app/store";
 import { addReservation } from "./features/reservationSlice";
@@ -9,9 +10,11 @@ import { addReservation } from "./features/reservationSlice";
 
 function App() {
 
-  const [reservationNameInput, setReservationNameInput] = useState("");
+const [reservationNameInput, setReservationNameInput] = useState("");
 
 const reservations = useSelector((state: RootState) => state.reservations.value);
+
+const customers = useSelector((state: RootState) => state.customers.value);
 
 const dispatch = useDispatch();
 
@@ -41,16 +44,9 @@ const handleAddReservation = () => {
           </div>
         </div>
         <div className="customer-food-container">
-          <div className="customer-food-card-container">
-            <p>Selena Gomez</p>
-            <div className="customer-foods-container">
-              <div className="customer-food"></div>
-              <div className="customer-food-input-container">
-                <input />
-                <button>Add</button>
-              </div>
-            </div>
-          </div>
+            {customers.map(customer => {
+              return <CustomerCard />
+            })}
         </div>
       </div>
     </div>
